d764a2963981b319a8efe469024743704ea5584b
Veeci <quangdv1@talent.apero.vn>
2026-03-12 10:16:40 +0700

    first commit

---
+++ composeApp/src/commonTest/kotlin/com/example/kmptraining/ComposeAppCommonTest.kt
@@ -0,0 +1,12 @@
+package com.example.kmptraining
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ComposeAppCommonTest {
+
+    @Test
+    fun example() {
+        assertEquals(3, 1 + 2)
+    }
+}
